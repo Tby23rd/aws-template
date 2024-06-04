d94ef76b8f74080f6bfef63352b1a581eaa8dd19
tby23rd <tabitha.maru@gmail.com>
2024-06-04 14:09:14 -0600

    Added a logout button

--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,19 @@
 "use client";
-
-import { useState, useEffect } from "react";
-import { generateClient } from "aws-amplify/data";
-import type { Schema } from "@/amplify/data/resource";
+import {useState,useEffect} from "react";
+import {generateClient} from "aws-amplify/data";
+import type {Schema} from "@/amplify/data/resource";
 import "./../app/app.css";
-import { Amplify } from "aws-amplify";
+import {Amplify} from "aws-amplify";
 import outputs from "@/amplify_outputs.json";
 import "@aws-amplify/ui-react/styles.css";
+import {Authenticator} from "@aws-amplify/ui-react";
 
 Amplify.configure(outputs);
 
-const client = generateClient<Schema>();
+const client=generateClient<Schema>();
 
 export default function App() {
-  const [todos, setTodos] = useState<Array<Schema["Todo"]["type"]>>([]);
+  const [todos,setTodos]=useState<Array<Schema["Todo"]["type"]>>([]);
 
   function listTodos() {
     client.models.Todo.observeQuery().subscribe({
@@ -21,9 +21,13 @@ export default function App() {
     });
   }
 
+  function deleteTodo(id: string) {
+    client.models.Todo.delete({id});
+  }
+
   useEffect(() => {
     listTodos();
-  }, []);
+  },[]);
 
   function createTodo() {
     client.models.Todo.create({
@@ -32,21 +36,30 @@ export default function App() {
   }
 
   return (
-    <main>
-      <h1>My todos</h1>
-      <button onClick={createTodo}>+ new</button>
-      <ul>
-        {todos.map((todo) => (
-          <li key={todo.id}>{todo.content}</li>
-        ))}
-      </ul>
-      <div>
-        🥳 App successfully hosted. Try creating a new todo.
-        <br />
-        <a href="https://docs.amplify.aws/nextjs/start/quickstart/nextjs-app-router-client-components/">
-          Review next steps of this tutorial.
-        </a>
-      </div>
-    </main>
+    <Authenticator>
+      {({signOut, user}) => (
+        <main>
+          <button onClick={signOut}>Sign out</button>
+        <h1>My todos</h1>
+        <button onClick={createTodo}>+ new</button>
+        <ul>
+          {todos.map((todo) => (
+            <li key={todo.id} className="p-6 space-x-6 flex flex-col">
+              <span className="text-lg font-bold py-8" key={todo.id} onClick={() => deleteTodo(todo.id)}>
+                x
+              </span>
+              <span className="text-blue-900 font-bold py-8" key={todo.id}>{todo.content}</span>
+
+            </li>
+          ))}
+        </ul>
+
+      </main>
+      )
+      
+      }
+
+    </Authenticator>
+
   );
 }
